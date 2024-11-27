f4255409c4ee7d57625aa3ad915d0be504e33c0e
Yasadu <ydesilva@umd.edu>
2024-11-27 15:41:55 -0500

    code to create a get request

--- construct_http_requests.py
+++ construct_http_requests.py
@@ -1,27 +1,27 @@
 HTTP_LINE_END = "\r\n"
+USER_AGENT = "Wget/1.21.3"
 
-
-def construct_GET_request(host_url, user_agent):
+def construct_GET_request(host_domain, host_port, path):
     request = ""
 
-    request_line = "GET / HTTP/1.1" + HTTP_LINE_END
+    request_line = "GET " + path + " HTTP/1.1" + HTTP_LINE_END
     request += request_line
 
-    host_line = "Host: " + host_url + HTTP_LINE_END
+    host_line = "Host: " + host_domain + ":" + str(host_port) + HTTP_LINE_END
     request += host_line
 
-    user_agent_line = "User-Agent: " + user_agent + HTTP_LINE_END
+    user_agent_line = "User-Agent: " + USER_AGENT + HTTP_LINE_END
     request += user_agent_line
 
     accept_line = "Accept: */*" + HTTP_LINE_END
     request += accept_line
 
     accept_encoding_line = "Accept-Encoding: identity" + HTTP_LINE_END
-    request += accept_encoding line
+    request += accept_encoding_line
 
     connection_line = "Connection: Keep-Alive" + HTTP_LINE_END
     request += connection_line
 
     request += HTTP_LINE_END
 
-    return request
+    return request.encode()
--- pysock.py
+++ pysock.py
@@ -1,8 +1,10 @@
 import socket
 import selectors
+import construct_http_requests
 from bcoding import bencode, bdecode
 
 def main():
+    '''
     torfile = open("tor-file-examples/cosmos-laundromat.torrent", 'rb')
     tde = bdecode(torfile)
     addrstr = tde['announce']
@@ -10,7 +12,18 @@ def main():
     ip_addr = addrsep[0] + addrsep[1]
     port = addrsep[2]
     print(port) # ensure main() is actually running when you execute ~/cmsc417-BitTorrent/venv/bin/python3 {FILENAME}.py
+    '''
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(("www.example.com", 80))
+
+    msg = construct_http_requests.construct_GET_request("example.com", 80, "/")
     
+    totalsent = 0
+    while totalsent < len(msg):
+        sent = sock.send(msg[totalsent:])
+        if sent == 0:
+            raise RuntimeError("socket connection broken")
+        totalsent = totalsent + sent
 
 if __name__=="__main__":
     main()
