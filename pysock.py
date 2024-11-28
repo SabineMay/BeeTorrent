36c38191a8ea4160352d2d799378cc5dac333efc
Yasadu <ydesilva@umd.edu>
2024-11-27 23:14:53 -0500

    added code to receive http response

--- pysock.py
+++ pysock.py
@@ -1,41 +1,23 @@
 import socket
 import selectors
 from torrent_get_requests import *
-
+from send_receive_message import *
+from bcoding import bencode, bdecode
 
 def main():
-    msg = create_torrent_get_request_from_file("tor-file-examples/kali-linux.torrent", 6881, event='started')
-
+    # connect to the tracker
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect(("bt1.archive.org", 6969))
-    
-    totalsent = 0
-    while totalsent < len(msg):
-        sent = sock.send(msg[totalsent:])
-        if sent == 0:
-            raise RuntimeError("socket connection broken")
-        totalsent = totalsent + sent
 
-    '''
-    torfile = open("tor-file-examples/cosmos-laundromat.torrent", 'rb')
-    tde = bdecode(torfile)
-    addrstr = tde['announce']
-    addrsep = addrstr.split(':')
-    ip_addr = addrsep[0] + addrsep[1]
-    port = addrsep[2]
-    print(port) # ensure main() is actually running when you execute ~/cmsc417-BitTorrent/venv/bin/python3 {FILENAME}.py
-    '''
-    # sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # sock.connect(("www.example.com", 80))
+    # send a get request to the tracker for this torrent
+    msg = create_torrent_get_request_from_file("tor-file-examples/kali-linux.torrent", 6881, event='started')
+    send_message(msg, sock)
 
-    # msg = construct_GET_request("example.com", 80, "/")
-    
-    # totalsent = 0
-    # while totalsent < len(msg):
-    #     sent = sock.send(msg[totalsent:])
-    #     if sent == 0:
-    #         raise RuntimeError("socket connection broken")
-    #     totalsent = totalsent + sent
+    # receive the response back, the payload of the http response is stored in resp
+    resp = recv_http_response(sock)
+
+    # decode and print 
+    print(bdecode(resp))
 
 if __name__=="__main__":
     main()
