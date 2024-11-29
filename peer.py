4ec172df5d95d24f0b64fff03ddd90c481f35865
pmiller2022 <pmiller8@umd.edu>
2024-11-29 11:24:54 -0500

    updating ig

--- peer.py
+++ peer.py
@@ -2,13 +2,23 @@ import socket
 import selectors
 import sys
 from bcoding import bencode, bdecode
+import hashlib
 
 # Test file for sending between peers
 # arguments -p port_num -s {optional, indicates server mode}
 
 def main():
-    ipa = "172.16.45.87"
+    torfile = open("tor-file-examples/cosmos-laundromat.torrent", 'rb')
+    tde = bdecode(torfile)
+    info_hash = hashlib.sha1(tde['info']['pieces'])
+    info_hash = info_hash.digest()
+
+
+
+
+    ipa = socket.gethostbyname(socket.gethostname())
     port = int(sys.argv[2])
+    '''
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -16,29 +26,29 @@ def main():
     if (len(sys.argv) > 3):
        sock.bind((ipa, port))
 
-       sock.listen(5)
+       sock.listen(100)
 
        (clisock, addr) = sock.accept()
 
-       msg = bytearray(4)
-       msg[0] = 0
-       msg[1] = 0
-       msg[2] = 0
-       msg[3] = 1
-       clisock.send(msg)
+       #length = (5).to_bytes(4, 'big')
+       pstr = "BitTorrent protocol"
+       pstrlen = len(pstr)
+       res = bytearray(8)
 
-       id = bytearray(1)
-       id[0] = 1
 
-       clisock.send(id)
+
+       #clisock.send(length)
+
+       clisock.close()
+       sock.close()
     
     # Client mode
     else:
         sock.connect((ipa, port))
 
-        mes = sock.recv(4)
+        #mes = sock.recv(4)
 
-        length = int.from_bytes(mes, "big")
+        #length = int.from_bytes(mes, "big")
 
         if length > 0:
             id = sock.recv(1)
@@ -46,11 +56,12 @@ def main():
             id = int.from_bytes(id)
 
             print(id)
-
-
-        print(length)
         
 
+
+        #print(length)
+        sock.close()
+    '''
     
     
 
